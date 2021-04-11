f763ceb91c9b7eccdd97f4e8cbce7403be8fd8ec
yerinko <yerin_o3o@naver.com>
2021-04-11 18:30:38 +0900

    [1000] A+B

---
+++ 입출력과 사칙연산/1000.js
@@ -0,0 +1,17 @@
+// 문제
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+//
+// 입력
+// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+//
+// 출력
+// 첫째 줄에 A+B를 출력한다.
+
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+// tip: parseInt -> 문자열을 정수로 바꾸는 함수입니다.
+const input = [1,2];
+const A = parseInt(input[0]);
+const B = parseInt(input[1]);
+console.log(A + B);
