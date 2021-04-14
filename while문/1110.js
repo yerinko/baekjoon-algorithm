26a45fab3f69221ecb64e334a17b6cae8124a7d1
yerinko <yerin_o3o@naver.com>
2021-04-14 23:49:14 +0900

    [1110] 더하기 사이클 🧐

--- while문/1110.js
+++ while문/1110.js
@@ -10,3 +10,29 @@
 //
 // 출력
 // 첫째 줄에 N의 사이클 길이를 출력한다.
+
+const input = ['55'];
+
+let num = Number(input);
+
+let cycle = 0;
+let temp = num;
+let step = 0;
+let isNotSame = true;
+
+while (isNotSame) {
+    cycle ++;
+    if ( temp < 10 ) {
+        step = temp%10;
+    } else {
+        step = Math.floor(temp / 10 ) + ( temp % 10 );
+    }
+
+    temp = ( temp % 10 ).toString() + ( step % 10 ).toString();
+    temp = Number(temp);
+
+    if (temp === num ) {
+        isNotSame = false;
+    }
+}
+console.log( cycle);
