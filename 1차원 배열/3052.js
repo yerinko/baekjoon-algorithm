63ffdd05a48a7a2583ca66d89c1e18d0c0ffeb82
yerinko <yerin_o3o@naver.com>
2021-04-23 23:52:48 +0900

    [3052] 나머지

--- 1차원 배열/3052.js
+++ 1차원 배열/3052.js
@@ -7,3 +7,24 @@
 //
 // 출력
 // 첫째 줄에, 42로 나누었을 때, 서로 다른 나머지가 몇 개 있는지 출력한다.
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let numbers = [];
+for (let i = 0; i < 10; i++) {
+    numbers.push(Number(input[i]));
+}
+
+let restNum = numbers.map(function(num) {
+    return num % 42;
+});
+
+let diffNum = [];
+restNum.forEach(function(num) {
+    if (diffNum.indexOf(num) === -1 ) {
+        diffNum.push(num);
+    }
+});
+
+console.log(diffNum.length);
