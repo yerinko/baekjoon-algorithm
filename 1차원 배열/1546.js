72a5b08d5b6a451f0f9a247360c0dfa0b00d95cc
yerinko <yerin_o3o@naver.com>
2021-04-25 23:53:44 +0900

    [1546] 평균

--- 1차원 배열/1546.js
+++ 1차원 배열/1546.js
@@ -8,3 +8,32 @@
 //
 // 출력
 // 첫째 줄에 새로운 평균을 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-2 이하이면 정답이다.
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let subject = Number(input[0]);
+
+let score = input[1].split(' ');
+let scoreNum = [];
+for (let i = 0; i < subject; i++) {
+    scoreNum.push(Number(score[i]));
+}
+
+let maxScore = scoreNum[0];
+for (let i = 1; i < scoreNum.length; i++) {
+    if (maxScore < scoreNum[i]) {
+        maxScore = scoreNum[i];
+    }
+}
+
+let newScore = [];
+newScore = scoreNum.map(function(score) {
+    return score / maxScore * 100;
+});
+
+let scoreSum = newScore.reduce(function(acc, cur) {
+    return acc + cur;
+});
+
+console.log(scoreSum / subject);
