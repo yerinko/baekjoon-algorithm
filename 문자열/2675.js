0bb70fb45fb00531dfc0730bb4b33425a4f2459a
yerinko <yerin_o3o@naver.com>
2021-05-05 08:46:33 +0900

    [2675] 문자열 반복

--- 문자열/2675.js
+++ 문자열/2675.js
@@ -12,19 +12,16 @@ const input = [2, '3 ABC', '5 /HTP'];
 let caseCount = Number(input[0]);
 let result = '';
 
-for ( let i = 1; i <= caseCount; i ++ ) {
-    let count = Number(input[i].split(' '))[0];
-    let cases = input[i].split(' ')[1];
-    // console.log('cases', cases)
-for (let j = 0; j < cases.length; j++) {
-    for (let k = 0; k < count; k++) {
-        result += cases[j];
+for (let i = 1; i <= caseCount; i++) {
+    let count = Number(input[i].split(' ')[0]);  // 각 케이스 당 문자 반복 횟수 (R)
+    let cases = input[i].split(' ')[1];  // 케이스 문자열 분리
+
+    for (let j = 0; j < cases.length; j++) {
+        for (let k=0; k < count; k++) {
+            result += cases[j]
+        }
     }
-
-}
-    console.log('result', result)
-// result += '\n';
+    result += '\n';
 }
 
-// console.log(result);
-// console.log(count, cases);
+console.log(result);
