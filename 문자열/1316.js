2dc2c2def39fe1fb824cc58ae5e85c99ddb89e1b
yerinko <yerin_o3o@naver.com>
2021-05-20 00:26:46 +0900

    [1316] 그룹 단어 체커

--- 문자열/1316.js
+++ 문자열/1316.js
@@ -8,4 +8,28 @@
 // 출력
 // 첫째 줄에 그룹 단어의 개수를 출력한다.
 
-const input = [ 3, 'haapy', 'new', 'yeyar '];
+const input = [ 3, 'happay', 'neaw', 'ayear'];
+const caseCount = Number(input[0]);
+let countGroupWord = 0;
+
+for( let i = 1; i <= caseCount; i ++ ) {
+    const word = input[i];
+    const letter = [];
+    let isGroupWord = true;
+
+    for (let j = 0; j < word.length; j++) {
+        if (letter.indexOf(word[j]) === -1) {
+            letter.push(word[j]);
+        } else {
+            if (letter.indexOf(word[j]) !== letter.length - 1) {
+            isGroupWord = false;
+            break;
+        }
+    }
+}
+    if (isGroupWord) {
+        countGroupWord += 1;
+    }
+}
+console.log(countGroupWord);
+
