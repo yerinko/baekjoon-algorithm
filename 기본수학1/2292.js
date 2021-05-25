4088d44a3b7a8109a02d0cb34dc9b396777eb72d
yerinko <yerin_o3o@naver.com>
2021-05-25 22:36:03 +0900

    [2292] 벌집

--- 기본수학1/2292.js
+++ 기본수학1/2292.js
@@ -6,3 +6,12 @@
 //
 // 출력
 // 입력으로 주어진 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나는지 출력한다.
+
+const N = 58;
+let counter = 1;
+let range = 1;
+
+while (range < N) {
+    range += counter++ * 6;
+}
+console.log(counter);
