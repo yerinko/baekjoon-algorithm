82732157d9ad60a40ec2de7bf48ec1502e4f3572
yerinko <yerin_o3o@naver.com>
2021-06-21 23:54:30 +0900

    [2839] 설탕 배달

--- 기본수학1/2775.js
+++ 기본수학1/2775.js
@@ -11,3 +11,20 @@
 //
 // 제한
 // 1 ≤ k, n ≤ 14
+
+let count = 0;
+
+while (true) {
+    if (input % 5 === 0) {
+        console.log(input / 5 + count);
+        break;
+    }
+
+    if (0 > input) {
+        console.log(-1);
+        break;
+    }
+
+    count++;
+    input -= 3;
+}
