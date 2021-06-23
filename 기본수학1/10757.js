004bfd1bf3e9c3eaf4e25366fb85355837cf811f
yerinko <yerin_o3o@naver.com>
2021-06-23 23:55:25 +0900

    [10757] 큰 수 A + B

--- 기본수학1/10757.js
+++ 기본수학1/10757.js
@@ -6,3 +6,20 @@
 //
 // 출력
 // 첫째 줄에 A+B를 출력한다.
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ').map(e => BigInt(e));
+    const result = input[0] + input[1];
+    console.log(result.toString());
+})
+    .on('close', function () {
+        process.exit();
+    });
